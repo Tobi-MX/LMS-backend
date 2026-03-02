ef7c28f9a4e0dd12f67fbaa348eaa9d7922770af
Tobi <Tobimustapha801@gmail.com>
2026-03-02 21:26:56 +0100

    Initial database set up

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const {MONGO_URI} = process.env
+        if (!MONGO_URI) throw new Error("MONGO_URI is not set")
+
+        const conn = await mongoose.connect(MONGO_URI)
+        console.log("MONGODB connected successfully", conn.connection.host)
+    } catch (error) {
+        console.error("Failed to connect to database", error)
+        process.exit(1)
+    }
+}
