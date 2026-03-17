a00ae1f85444ab77117a8e65e9541d9202bafbbe
Tobi <Tobimustapha801@gmail.com>
2026-03-17 16:34:03 +0100

    Created the initial enrollment model

---
+++ src/models/Enrollment.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const enrollmentSchema = new mongoose.Schema({
+
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+        required: true
+    },
+
+    status: {
+        type: String,
+        enum: ["active", "completed"],
+        default: "active"
+    }
+
+}, { timestamps: true });
+
+enrollmentSchema.index({ user: 1, course: 1 }, { unique: true });
+
+export const Enrollment = mongoose.model("Enrollment", enrollmentSchema);
