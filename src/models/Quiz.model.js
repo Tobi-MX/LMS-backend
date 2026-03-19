2ca33fa04890cd60e22f41e6e76aecf89a15c3a2
Tobi <Tobimustapha801@gmail.com>
2026-03-19 18:08:51 +0100

    Created initial Quiz/Question model

---
+++ src/models/Quiz.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose"
+
+const questionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: true
+    },
+
+    options: [{
+        type: String,
+        required: true
+    }],
+
+    correctAnswer: {
+        type: Number, // index of correct option
+        required: true
+    }
+});
+
+const quizSchema = new mongoose.Schema({
+    lesson: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lesson",
+        required: true
+    },
+
+    questions: [questionSchema],
+
+    passingScore: {
+        type: Number,
+        default: 50
+    }
+}, { timestamps: true });
+
+export const Quiz = mongoose.model("Quiz", quizSchema);
